4f74ea88e60c05ec09e49af794f71764ee1dcdec
falufriSebastianLiljedal <79262917+falufriSebastianLiljedal@users.noreply.github.com>
2025-09-03 13:07:11 +0200

    Created switchcase

---
+++ Grund/switch.cs
@@ -0,0 +1,19 @@
+int choice = Convert.ToInt32(Console.ReadLine());
+switch (choice)
+{
+    case 0:
+    Console.WriteLine("Choice is 0");
+    break;
+
+    case 1:
+    Console.WriteLine("Choice is 1");
+    break;
+
+    case 2:
+    Console.WriteLine("Choice is 2");
+    break;
+
+    default:
+    Console.WriteLine("This one is default");
+    break;
+}
